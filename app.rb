61f789e90e0a629b178e54bf6bcfe138f2a996db
Mark May <mamark90@gmail.com>
2020-08-30 13:04:40 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
 
   post '/' do
     #text_from_user = params[:user_text]
-    TextAnalyzer.new(text_from_user)
+    @text_analyzer = TextAnalyzer.new(params[:user_text])
 
     erb :results
   end
